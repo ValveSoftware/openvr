e19368a8faea73e00b629a9f9257649f92311f83
Heresy <kheresy@gmail.com>
2016-05-02 10:38:52 +0800

    second edition
    1. add a floating overlay with controller
    2. show basic controller information

--- MyProgram/2DOverlay/Source.cpp
+++ MyProgram/2DOverlay/Source.cpp
@@ -1,10 +1,11 @@
 // A test program to use 'overlay' in OpenVR
 
 // STD Headers
+#include <chrono>
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <thread>
-#include <chrono>
 
 // OpenVR
 #include <openvr.h>
@@ -56,61 +57,97 @@ int main(int argc, char** argv)
 
 	// Get VR Overlay
 	vr::IVROverlay* pVROverlay = vr::VROverlay();
-	vr::VROverlayHandle_t handleOverlay = 0;
+	vr::VROverlayHandle_t hdlBigImage = 0, hdlInfoImage = 0;
+	cv::Mat imInfo(100, 300, CV_8UC4);
 	if (pVROverlay != nullptr)
 	{
-		vr::HmdMatrix34_t matTransform = {
-			1,0,0,0,
-			0,1,0,0,
-			0,0,1,-3 };
-
-		// create overlay
-		if (pVROverlay->CreateOverlay("The-Key", "The-Name", &handleOverlay) == vr::VROverlayError_None)
+		// create overlay 1
+		if (pVROverlay->CreateOverlay("BigImage", "A Big Image wall", &hdlBigImage) == vr::VROverlayError_None)
 		{
 			// Size and position
-			pVROverlay->SetOverlayWidthInMeters(handleOverlay, 3.0f);
-			//pVROverlay->SetOverlayTransformAbsolute(handleOverlay, vr::TrackingUniverseSeated, &matTransform);
-			pVROverlay->SetOverlayTransformTrackedDeviceRelative(handleOverlay, 1, &matTransform);
+			pVROverlay->SetOverlayWidthInMeters(hdlBigImage, 3.0f);
+			vr::HmdMatrix34_t matTransform = {
+				1,0,0,0,
+				0,1,0,0,
+				0,0,1,-3 };
+			pVROverlay->SetOverlayTransformAbsolute(hdlBigImage, vr::TrackingUniverseSeated, &matTransform);
 
 			// non-used
-			pVROverlay->SetOverlayColor(handleOverlay, 1.0f, 1.0f, 1.0f);
-			pVROverlay->SetOverlayAlpha(handleOverlay, 1.0f);
-			pVROverlay->SetOverlayInputMethod(handleOverlay, vr::VROverlayInputMethod_Mouse);
+			pVROverlay->SetOverlayColor(hdlBigImage, 1.0f, 1.0f, 1.0f);
+			pVROverlay->SetOverlayAlpha(hdlBigImage, 1.0f);
+			pVROverlay->SetOverlayInputMethod(hdlBigImage, vr::VROverlayInputMethod_Mouse);
 
 			// show it!
-			pVROverlay->ShowOverlay(handleOverlay);
+			pVROverlay->ShowOverlay(hdlBigImage);
+
+			// set image, load from file
+			pVROverlay->SetOverlayFromFile(hdlBigImage, "E:\\testb.jpg");
+		}
+
+
+		// create overlay 2
+		if (pVROverlay->CreateOverlay("InfoImage", "Information Image", &hdlInfoImage) == vr::VROverlayError_None)
+		{
+			// Size and position
+			pVROverlay->SetOverlayWidthInMeters(hdlInfoImage, 0.1f);
+			vr::HmdMatrix34_t matTransform = {
+				1,0,0,0,
+				0,1,0,0.1,
+				0,0,1,0 };
+			pVROverlay->SetOverlayTransformTrackedDeviceRelative(hdlInfoImage, 1, &matTransform);
+
+			// non-used
+			pVROverlay->SetOverlayColor(hdlInfoImage, 1.0f, 1.0f, 1.0f);
+			pVROverlay->SetOverlayAlpha(hdlInfoImage, 1.0f);
+			pVROverlay->SetOverlayInputMethod(hdlInfoImage, vr::VROverlayInputMethod_None);
+
+			// show it!
+			pVROverlay->ShowOverlay(hdlInfoImage);
+
+			// set image image
+			// This code faile if image too large
+			// Should be the bug of OpenVR: https://github.com/ValveSoftware/openvr/issues/46
+			pVROverlay->SetOverlayRaw(hdlInfoImage, imInfo.data, imInfo.cols, imInfo.rows, imInfo.channels());
 		}
 	}
 
-	if(false)
-	{
-		cv::Mat imgFile = cv::imread("E:\\testb.jpg");
-		cv::Mat imgImage;
-		cv::cvtColor(imgFile, imgImage, cv::COLOR_BGR2RGBA);
-
-		// This code faile if image too large
-		// Should be the bug of OpenVR: https://github.com/ValveSoftware/openvr/issues/46
-		pVROverlay->SetOverlayRaw(handleOverlay, imgImage.data, imgImage.cols, imgImage.rows, imgImage.channels());
-	}
-	else
-	{
-		pVROverlay->SetOverlayFromFile(handleOverlay, "E:\\testb.jpg");
-	}
-
+	vr::VRControllerState_t mCState;
+	vr::TrackedDevicePose_t mCPose;
 	while (true)
 	{
-		// tell OpenVR to make some events for us
+		imInfo.setTo(128);
 		for (vr::TrackedDeviceIndex_t uDeviceIdx = 1; uDeviceIdx < vr::k_unControllerStateAxisCount; ++uDeviceIdx)
 		{
-			if (pVROverlay->HandleControllerOverlayInteractionAsMouse(handleOverlay, uDeviceIdx))
+			if (pVRSystem->IsTrackedDeviceConnected(uDeviceIdx))
+			{
+				// tell OpenVR to make some events for us
+				pVROverlay->HandleControllerOverlayInteractionAsMouse(hdlBigImage, uDeviceIdx);
+
+				// get information of controller
+				pVRSystem->GetControllerStateWithPose(vr::TrackingUniverseStanding, uDeviceIdx, &mCState, &mCPose);
+
+				std::ostringstream oss;
+				oss << uDeviceIdx << " (" << mCPose.eTrackingResult << ") - " << mCState.ulButtonPressed;
+				cv::putText(imInfo, oss.str(), cv::Point( 2, 50 * uDeviceIdx - 10), cv::FONT_HERSHEY_PLAIN, 2, cv::Scalar( 0,0,0,255), 5 );
+			}
+			else
+			{
 				break;
+			}
+		}
+
+		// update hdlInfoImage, may flick
+		if (pVROverlay->IsOverlayVisible(hdlInfoImage))
+		{
+			pVROverlay->SetOverlayRaw(hdlInfoImage, imInfo.data, imInfo.cols, imInfo.rows, imInfo.channels());
 		}
 
 		vr::VREvent_t eVREvent;
-		while (pVROverlay->PollNextOverlayEvent(handleOverlay, &eVREvent, sizeof(eVREvent)))
+		while (pVROverlay->PollNextOverlayEvent(hdlBigImage, &eVREvent, sizeof(eVREvent)))
 		{
 		}
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 	
 	//vr::IVRCompositor* pCompositor = vr::VRCompositor();
