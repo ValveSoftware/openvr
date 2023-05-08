9c6bfdb80f3b14202aa9c762afc8478135ff8f0f
Dmitry Tsarevich <dimhotepus@gmail.com>
2023-05-08 15:08:57 +0300

    [json]: Correctly print unsigned argument

--- src/jsoncpp.cpp
+++ src/jsoncpp.cpp
@@ -4183,7 +4183,7 @@ std::string valueToString(double value, bool useSpecialFloats, unsigned int prec
   int len = -1;
 
   char formatString[6];
-  sprintf(formatString, "%%.%dg", precision);
+  sprintf(formatString, "%%.%ug", precision);
 
   // Print into the buffer. We need not request the alternative representation
   // that always has a decimal point because JSON doesn't distingish the
